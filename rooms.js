433acccb00354c5710b1c5b1fdc2e55af6a88043
karnurmax <karnurmax@mail.ru>
2023-06-15 04:26:59 +0600

    convert function to class User

--- rooms.js
+++ rooms.js
@@ -1,25 +1,25 @@
 const rooms = [];
 const users = [];
+
 const USER_STATE = {
     CREATED: 'created',
     READY: 'ready',
     FINISHED: 'finished'
 }
-function User(socket) {
-    this.socket = socket;
-    this.score = 0;
-    this.state = USER_STATE.CREATED;
+class User {
+    constructor(socket) {
+        this.socket = socket;
+        this.score = 0;
+        this.state = USER_STATE.CREATED;
+    }
 }
 function joinRoom(id, socket) {
-    // Find the first room with usersCount less than 2
     let room = rooms.find(room => room.users.length < 2);
     const user = new User(socket);
     users.push(user);
     if (room) {
-        // If an existing room is found, add the socket to it
         room.users.push(user);
     } else {
-        // If no existing room is found, create a new room with the socket
         room = {
             id,
             users: [user]
